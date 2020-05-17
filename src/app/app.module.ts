d12f632601f49832d07e00c71ce499f5d8071c79
puku0x <puku0x@gmail.com>
2020-05-17 15:05:20 +0900

    remove http client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
 import { ScullyLibModule } from '@scullyio/ng-lib';
 
 import { SharedModule } from './shared';
@@ -13,7 +12,6 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule,
     AppRoutingModule,
     ScullyLibModule,
     SharedModule,
